8a4a0bb5e88dca634dec3877acc7c06cc26dc9ee
charukiewicz <c.charukiewicz@gmail.com>
2018-12-01 03:21:13 -0600

    added cabal file, stack.yaml file

--- day-01/Day01.hs
+++ day-01/Day01.hs
@@ -6,14 +6,13 @@ main :: IO ()
 main = do
     frequencies <- readFile "frequencies.txt"
     let frequencyList = cleanFrequencyInput frequencies
-        -- Bangs (!) are used so that the output is printed after evalulation
-        !resultingFrequency = sum frequencyList
-        !repeatedFrequency = findRepeatedFreq $ cycle frequencyList
+        resultingFrequency = sum frequencyList
+        repeatedFrequency = findRepeatedFreq $ cycle frequencyList
 
     putStrLn $ "Part 1: The resulting frequency is " ++ show resultingFrequency
     putStrLn $ "Part 2: The first repeated frequency is " ++ show repeatedFrequency
 
--- | Takes an input string from file and removes 
+-- | Takes an input string from file and removes
 -- the '+' character, then converts to an Int
 cleanFrequencyInput :: String -> [Int]
 cleanFrequencyInput freqInput =
